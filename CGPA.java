9274809000a2919c8bf3a8d55cac6a91a0cd4cc1
Darshan1223 <113288560+Darshan1223@users.noreply.github.com>
2023-09-12 11:30:10 +0530

    Create CGPA.java

---
+++ CGPA.java
@@ -0,0 +1,19 @@
+import java.io*;
+import java.util.*;
+public class CGPAProgram  
+{     
+public static void main (String args[])  
+    {     
+    double English, Language, Maths, Science, SocialStudy, CGPA, CGPAper ;
+	Scanner sc = new Scanner (System.in);
+	
+    English = sc.nextDouble();  
+    Maths = sc.nextDouble();
+    Science = sc.nextDouble();
+    SocialStudy = sc.nextDouble();
+	Language = sc.nextDouble();
+    CGPA = (English + Language + Maths + Science + SocialStudy)/(5.0);  
+    CGPAper = (float)(9.5 * (CGPA));  
+    System.out.println(" CGPA Percentage is:  "+CGPAper);  
+    }  
+}  
